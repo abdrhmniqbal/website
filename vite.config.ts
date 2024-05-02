a28113f37c32f96384bdc53edbca1beb6b6da073
iblabd <iqbalabdurrahman04@gmail.com>
2024-05-02 13:29:23 +0700

    chore: add deployment example to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
+import { vercelPreset } from '@vercel/remix/vite'
 import { remixDevTools } from 'remix-development-tools'
 import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     remixDevTools(),
     remix({
+      presets: [vercelPreset()],
       appDirectory: './src/app',
       routes: async defineRoutes => {
         return flatRoutes('routes', defineRoutes, { appDir: './src/app' })
